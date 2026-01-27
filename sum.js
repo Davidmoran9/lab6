5b69fdd80ee2ae2a4f69d97098c0955526bcf8cf
Davidmoran9 <davidleonel0408@gmail.com>
2026-01-27 10:27:54 -0500

    Prueba resta

--- sum.js
+++ sum.js
@@ -2,3 +2,8 @@ function sum(a, b) {
     return a + b;
 }
 module.exports = sum;
+
+function rest(a,b) {
+    return a - b;
+}
+module.exports = rest;
--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,8 @@ test('adds 1 + 2 debe ser 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+const rest = require('./sum');
+test('resta 5 - 2 debe ser 3', () => {
+  expect(rest(5, 2)).toBe(3);
+});
+
